cc94ba645bcfcb62529f0dcf79801b926fce97bd
daFruit <milan.jezovsek@gmail.com>
2025-04-14 13:06:33 +0200

    Revert "added pages"
    This reverts commit 8aaec58fbd5327a6338ee1171e28fb5c91d3aa7e.

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,10 +3,7 @@ import { Route, Routes } from "react-router";
 import Home from "./pages/Home.tsx";
 
 export const AppRoutes = () => (
-	<Routes>
-		<Route
-			path="/"
-			element={<Home />}
-		/>
-	</Routes>
+  <Routes>
+    <Route path="/" element={<Home />} />
+  </Routes>
 );
