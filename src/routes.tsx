8aaec58fbd5327a6338ee1171e28fb5c91d3aa7e
daFruit <milan.jezovsek@gmail.com>
2025-04-14 13:00:50 +0200

    added pages
    + Docs.tsx
    + Landing.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,7 +3,10 @@ import { Route, Routes } from "react-router";
 import Home from "./pages/Home.tsx";
 
 export const AppRoutes = () => (
-  <Routes>
-    <Route path="/" element={<Home />} />
-  </Routes>
+	<Routes>
+		<Route
+			path="/"
+			element={<Home />}
+		/>
+	</Routes>
 );
