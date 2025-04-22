ceb117a91d5e6263a3d1e1da11f62330a2dd6123
Leo Kling <leo.kling.info@gmail.com>
2025-04-22 14:14:52 +0200

    landing page content

--- app/routes/Home.tsx
+++ app/routes/Home.tsx
@@ -139,6 +139,49 @@ const Home = () => {
           </div>
         </main>
       </div>
+
+      <section className="flex items-center justify-center bg-white">
+        <div className="flex h-full w-full flex-col p-12">
+          <h1 className="font-pixel pb-12 text-4xl font-bold">
+            Learn these skills to become a dumpster diver
+          </h1>
+          <div className="flex items-start justify-center gap-32">
+            <div className="flex flex-col gap-4 text-center">
+              <div className="bg-jam-600 h-32 w-32 rounded-full" />
+              Computer
+              <br />
+              Animation
+            </div>
+            <div className="flex flex-col gap-4 text-center">
+              <div className="bg-jam-600 h-32 w-32 rounded-full" />
+              Eating Trash
+            </div>
+            <div className="flex flex-col gap-4 text-center">
+              <div className="bg-jam-600 h-32 w-32 rounded-full" />
+              Raiding The <br />Trash
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="flex h-96 items-center justify-center bg-blue-950">
+        <div className="flex h-full w-full max-w-2xl flex-col items-center justify-center gap-4">
+          <h1 className="font-pixel text-4xl font-bold text-white">
+            Join the community
+          </h1>
+          <p className="text-sm text-white text-center">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
+          </p>
+          <a
+            href="https://discord.gg/yourdiscordlink"
+            className="bg-jam-600 rounded-full px-6 py-3 text-lg font-bold text-white"
+          >
+            Join Discord
+          </a>
+        </div>
+      </section>
       <footer className="p-4 text-center text-black">
         <p>haha du hast das hier gelesen</p>
       </footer>
