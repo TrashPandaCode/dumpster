e7ca2392f0da0c5cf75713999e5cb572feb8574e
Leo Kling <leo.kling.info@gmail.com>
2025-04-22 11:03:32 +0200

    fix canvas

--- app/lib/game/core/kaplayCtx.ts
+++ app/lib/game/core/kaplayCtx.ts
@@ -1,12 +1,18 @@
 import kaplay from "kaplay";
 
 export default function makeKaplayCtx() {
+  const canvas = document.getElementById("game-canvas") as HTMLCanvasElement;
+  if (!canvas) {
+    console.error("Canvas element not found");
+    return;
+  }
+  
   return kaplay({
     global: false,
     pixelDensity: 2,
     touchToMouse: true,
     debug: false,
     debugKey: "f1",
-    canvas: document.getElementById("game")! as HTMLCanvasElement,
+    canvas: canvas,
   });
 }
--- app/routes/Game.tsx
+++ app/routes/Game.tsx
@@ -1,22 +1,25 @@
+import { useEffect } from "react";
 import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
 
 import initGame from "~/lib/game/core/initGame";
 
 const Game = () => {
+  useEffect(() => {
+    initGame();
+  }, []);
+
   return (
     <PanelGroup direction="horizontal">
       {/* autoSaveId="main-layout" */}
-      <Panel id="game" minSize={25} order={1}>
-        <canvas id="game" className="w-1/2"></canvas>
+      <Panel id="game-panel" minSize={25} order={1}>
+        <canvas id="game-canvas" className="w-1/2"></canvas>
       </Panel>
       <PanelResizeHandle className="w-2 bg-blue-800" />
-      <Panel id="nodes" minSize={25} order={2}>
+      <Panel id="nodes-panel" minSize={25} order={2}>
         <div className="w-1/2">Test</div>
       </Panel>
     </PanelGroup>
   );
 };
-
-initGame();
 
 export default Game;
