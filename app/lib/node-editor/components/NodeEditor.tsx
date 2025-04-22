224fb89aaf17476b09b95fa6a8911beda50bdbc9
Galileo Galilei <hypergameslp@gmail.com>
2025-04-22 14:27:07 +0200

    value node

--- app/lib/node-editor/components/NodeEditor.tsx
+++ app/lib/node-editor/components/NodeEditor.tsx
@@ -1,19 +1,23 @@
 import { Background, Controls, ReactFlow } from "@xyflow/react";
 import React from "react";
 
+import Value from "~/lib/node-editor/nodes-types/math-float/Value";
+
 import "@xyflow/react/dist/style.css";
 
 const NodeEditor = () => {
   const nodes = [
     {
       id: "1",
+      type: "Value",
       position: { x: 0, y: 0 },
       data: { label: "Hello" },
     },
   ];
+  const nodeTypes = { Value }; // Define your custom node types here
   return (
     <div style={{ height: "100%" }}>
-      <ReactFlow nodes={nodes}>
+      <ReactFlow nodes={nodes} nodeTypes={nodeTypes}>
         <Background />
         <Controls />
       </ReactFlow>
