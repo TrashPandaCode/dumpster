e8bbef8c8c3c94a48df2b0de70afa88f7525bccb
Leo Kling <leo.kling.info@gmail.com>
2025-04-24 18:59:52 +0200

    store example

--- app/lib/game/core/initGame.ts
+++ app/lib/game/core/initGame.ts
@@ -1,13 +1,20 @@
+import { useBearStore } from "~/lib/zustand/test";
 import { getKaplayCtx } from "./kaplayCtx";
 
 export default async function initGame(canvas: HTMLCanvasElement) {
   const k = getKaplayCtx(canvas);
 
   k.loadBean();
-  const bean = k.add([k.sprite("bean"), k.pos(0, 0), "bean"]);
-  bean.tag("bean");
+  const bean = k.add([k.sprite("bean"), k.pos(0, 0)]);
+  k.add([k.sprite("bean"), k.pos(20, 20)]);
+  
 
-  k.onUpdate("bean", (b) => {
-    b.moveBy(1, 1);
+  k.onUpdate(() => {
+    const x = useBearStore.getState().bears;
+    bean.moveBy(x, 0);
+
+    if (bean.pos.x > k.width()) {
+      bean.pos.x = 0;	
+    }
   });
 }
