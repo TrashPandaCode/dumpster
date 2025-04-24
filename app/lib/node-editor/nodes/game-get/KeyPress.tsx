cf62e46ff139864c83b6cd1dc7e6805c15c5216c
JonathanKr <post.david04@gmail.com>
2025-04-24 19:37:20 +0200

    test key press

---
+++ app/lib/node-editor/nodes/game-get/KeyPress.tsx
@@ -0,0 +1,33 @@
+import { Position, useKeyPress, useReactFlow } from "@xyflow/react";
+import { memo, useEffect } from "react";
+
+import Body from "../../node-components/Body";
+import Header from "../../node-components/Header";
+import LabelHandle from "../../node-components/LabelHandle";
+
+const KeyPress = memo(({ id }: { id: string }) => {
+  const { updateNodeData } = useReactFlow();
+  const spacePressed = useKeyPress("d");
+  console.log(spacePressed);
+
+  useEffect(() => {
+    updateNodeData(id, { value: +spacePressed });
+  }, [spacePressed]);
+
+  return (
+    <div className="min-w-48">
+      <Header>KeyPress (Test)</Header>
+      <Body>
+        <div className="flex w-full justify-end gap-2">
+          <LabelHandle
+            id="result-handle"
+            position={Position.Right}
+            label="Value"
+          />
+        </div>
+      </Body>
+    </div>
+  );
+});
+
+export default KeyPress;
--- app/lib/node-editor/nodes/node-types.ts
+++ app/lib/node-editor/nodes/node-types.ts
@@ -1,6 +1,7 @@
 import Display from "./--debug/Display";
+import KeyPress from "./game-get/KeyPress";
 import Time from "./game-get/Time";
 import Add from "./math-float/Add";
 import Value from "./math-float/Value";
 
-export const nodeTypes = { Value, Add, Display, Time };
+export const nodeTypes = { Value, Add, Display, Time, KeyPress };
