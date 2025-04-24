35cfa417f9c1f94ef6e8751d9784b7b1fbf18619
Leo Kling <leo.kling.info@gmail.com>
2025-04-24 18:33:48 +0200

    game: static movement

--- app/lib/game/core/initGame.ts
+++ app/lib/game/core/initGame.ts
@@ -1,5 +1,12 @@
 import { getKaplayCtx } from "./kaplayCtx";
 
 export default async function initGame(canvas: HTMLCanvasElement) {
-  getKaplayCtx(canvas);
+  const k = getKaplayCtx(canvas);
+
+  k.loadBean();
+  const bean = k.add([k.sprite("bean"), k.pos(0, 0), "bean"]);
+
+  k.onUpdate(() => {
+    bean.moveBy(1, 1);
+  })
 }
