0c7b537c1c7b46560942b30eb988fb4390cde498
dklein00 <131988668+dklein00@users.noreply.github.com>
2025-04-26 16:10:09 +0200

    Test level with basic gravity + movement

--- app/lib/game/core/initGame.ts
+++ app/lib/game/core/initGame.ts
@@ -5,21 +5,60 @@ import { useTimeStore } from "~/lib/zustand/time";
 export default async function initGame(canvas: HTMLCanvasElement) {
   const k = getKaplayCtx(canvas);
 
-  k.loadBean();
-  const bean = k.add([k.sprite("bean"), k.pos(0, 0)]);
-  k.add([k.sprite("bean"), k.pos(20, 20)]);
+  k.setBackground(100, 100, 100);
 
-  k.onUpdate(() => {
+  //Set Gravity and define Gravity direction
+  k.setGravity(980);
+  k.setGravityDirection(k.vec2(0, 1));
+  
+
+  k.loadBean(); //Load Bean sprite
+  const bean = k.add([
+    k.sprite("bean"), 
+    k.pos(100, k.height()- 100), 
+    k.area(), 
+    k.body({mass: 2}),
+    "playerBean",
+  ]);
+  const beanState = {
+    isGrounded: false,
+    canJump: true,
+    jumpForce: 500
+};
+
+  //Create "Floor" Component
+  const floor = k.add([
+    k.rect(k.width(), 5),
+    k.pos(0, k.height()-5),
+    k.color(255, 200, 200),
+    k.area(),
+    k.body({ isStatic: true }),
+    "floor"
+  ]);
+
+  //Game Loop, runs at 60 frames per second
+  k.onFixedUpdate(() => {
     const setTime = useTimeStore.getState().setTime;
     const time = k.time();
     setTime(time);
 
+    //Move
     const x = useDebugStore.getState().xpos;
+    //bean.addForce(k.vec2(x, 0)); //scuffed, needs max velocity function
 
-    bean.moveBy(x, 0);
+    //Jump
+    if((x > 0) && (beanState.canJump == true)){ //Provisionary input until we have proper export nodes
+      bean.jump(beanState.jumpForce);
+      beanState.canJump = false;
+      beanState.isGrounded = false;
+    }
 
     if (bean.pos.x > k.width()) {
       bean.pos.x = 0;
+    }
+    if (bean.isColliding(floor)) {
+      beanState.isGrounded = true;
+      beanState.canJump = true;
     }
   });
 }
