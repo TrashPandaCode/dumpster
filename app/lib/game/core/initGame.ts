d2dd5ee4075356e6610281b5c20742670d505bc1
JonathanKr <post.david04@gmail.com>
2025-04-26 16:21:28 +0200

    render game once in strict mode

--- app/lib/game/core/initGame.ts
+++ app/lib/game/core/initGame.ts
@@ -1,8 +1,13 @@
 import { useDebugStore } from "~/lib/zustand/debug";
-import { getKaplayCtx } from "./kaplayCtx";
 import { useTimeStore } from "~/lib/zustand/time";
+import { getKaplayCtx } from "./kaplayCtx";
+
+let first = true; //TODO: remove just for react strict mode
 
 export default async function initGame(canvas: HTMLCanvasElement) {
+  if (!first) return; //TODO: remove just for react strict mode
+  first = false; //TODO: remove just for react strict mode
+
   const k = getKaplayCtx(canvas);
 
   k.setBackground(100, 100, 100);
@@ -10,30 +15,29 @@ export default async function initGame(canvas: HTMLCanvasElement) {
   //Set Gravity and define Gravity direction
   k.setGravity(980);
   k.setGravityDirection(k.vec2(0, 1));
-  
 
   k.loadBean(); //Load Bean sprite
   const bean = k.add([
-    k.sprite("bean"), 
-    k.pos(100, k.height()- 100), 
-    k.area(), 
-    k.body({mass: 2}),
+    k.sprite("bean"),
+    k.pos(100, k.height() - 100),
+    k.area(),
+    k.body({ mass: 2 }),
     "playerBean",
   ]);
   const beanState = {
     isGrounded: false,
     canJump: true,
-    jumpForce: 500
-};
+    jumpForce: 500,
+  };
 
   //Create "Floor" Component
   const floor = k.add([
     k.rect(k.width(), 5),
-    k.pos(0, k.height()-5),
+    k.pos(0, k.height() - 5),
     k.color(255, 200, 200),
     k.area(),
     k.body({ isStatic: true }),
-    "floor"
+    "floor",
   ]);
 
   //Game Loop, runs at 60 frames per second
@@ -47,7 +51,8 @@ export default async function initGame(canvas: HTMLCanvasElement) {
     //bean.addForce(k.vec2(x, 0)); //scuffed, needs max velocity function
 
     //Jump
-    if((x > 0) && (beanState.canJump == true)){ //Provisionary input until we have proper export nodes
+    if (x > 0 && beanState.canJump == true) {
+      //Provisionary input until we have proper export nodes
       bean.jump(beanState.jumpForce);
       beanState.canJump = false;
       beanState.isGrounded = false;
