9423eeff50a0b69c1923c2211e22fe5f291f851f
dklein00 <131988668+dklein00@users.noreply.github.com>
2025-04-26 19:33:55 +0200

    back button on game page

--- app/routes/Game.tsx
+++ app/routes/Game.tsx
@@ -1,9 +1,11 @@
 import { DragHandleDots2Icon } from "@radix-ui/react-icons";
 import { useEffect, useRef } from "react";
 import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
+import { NavLink } from "react-router";
 
 import initGame from "~/lib/game/core/initGame";
 import NodeEditor from "~/lib/node-editor/NodeEditor";
+import arrowLeftIcon from "../assets/arrow_left.svg";
 
 const Game = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -20,6 +22,16 @@ const Game = () => {
     <PanelGroup direction="horizontal">
       {/* autoSaveId="main-layout" */}
       <Panel id="game-panel" minSize={25} order={1}>
+        <NavLink 
+          to="/" 
+          className="absolute top-4 left-1 z-10 py-2 px-4"
+        >
+          <img 
+              className="absolute z-1 cursor-pointer transition-transform hover:scale-110"
+              src={arrowLeftIcon}
+              alt="Go Back"
+          />
+        </NavLink>
         <canvas id="game-canvas" ref={canvasRef} className="w-1/2"></canvas>
       </Panel>
       <PanelResizeHandle className="flex w-2 items-center justify-center bg-slate-800">
