dd039ecd1666a8352605ed3e4b0a334eb5db2e3d
Leo Kling <leo.kling.info@gmail.com>
2025-05-01 13:08:38 +0200

    add home link

--- app/lib/core/components/Header.tsx
+++ app/lib/core/components/Header.tsx
@@ -4,9 +4,9 @@ const Header = () => {
   return (
     <header className="px-8">
       <nav className="flex items-center gap-8 p-4">
-        <div className="items-left font-pixel z-1 flex flex-col text-2xl font-bold">
+        <NavLink to="/" className="items-left font-pixel z-1 flex flex-col text-2xl font-bold">
           Dumpster Diving
-        </div>
+        </NavLink>
         <NavLink
           to="/docs"
           className="hover:text-jam-600 transition-all duration-300"
