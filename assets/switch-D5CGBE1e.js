f2e8b0882d3feac1899610db62edf84ce8eab229
ecrax <ecrax@users.noreply.github.com>
2025-05-01 16:29:32 +0000

    deploy: 9707242240b3b71b5019d71afa23dd057444efbd

---
+++ assets/switch-D5CGBE1e.js
@@ -0,0 +1,2 @@
+import{v as n}from"./chunk-AYJ5UCUI-BvwBlbwa.js";const r={title:"Switch"},a=`<h1>Switch</h1>
+`,i=function(t){return Object.keys(t).forEach(function(e){t[e]}),n.createElement("div",null,n.createElement("h1",null,"Switch"))};export{i as ReactComponent,r as attributes,a as html};
