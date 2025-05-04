c3fe07f3436e8dd03ed671ffc50c46a3498816f1
MilanJzo <MilanJzo@users.noreply.github.com>
2025-05-04 13:31:16 +0000

    deploy: 925dc17b370b5b4f52a9219ae44b34542930cfda

--- assets/debug-display-DCie2_U9.js
+++
@@ -1,2 +0,0 @@
-import{v as e}from"./chunk-AYJ5UCUI-BvwBlbwa.js";const c={title:"Debug-Display"},l=`<h1>Debug-Display</h1>
-`,r=function(t){return Object.keys(t).forEach(function(n){t[n]}),e.createElement("div",null,e.createElement("h1",null,"Debug-Display"))};export{r as ReactComponent,c as attributes,l as html};
--- assets/time-sh3uOf1k.js
+++
@@ -1,2 +0,0 @@
-import{v as e}from"./chunk-AYJ5UCUI-BvwBlbwa.js";const m={title:"Time"},r=`<h1>Time</h1>
-`,a=function(t){return Object.keys(t).forEach(function(n){t[n]}),e.createElement("div",null,e.createElement("h1",null,"Time"))};export{a as ReactComponent,m as attributes,r as html};
