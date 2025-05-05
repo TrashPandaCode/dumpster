d03d103dcedac9f0e3f6b0d10a4f1c5ca33e53cc
JonathanKr <post.david04@gmail.com>
2025-05-05 23:02:27 +0200

    remove group node

--- app/lib/node-editor/solutions/debug.ts
+++ app/lib/node-editor/solutions/debug.ts
@@ -6,18 +6,10 @@ export const debugNodes: Node[] = [
     id: uuidv4(),
     type: "Value",
     position: { x: 0, y: 0 },
-    data: { value: 0 },
-  },
-  {
-    id: "2",
-    type: "group",
-    position: { x: 0, y: 100 },
-    style: { width: 300, height: 400 },
     data: {},
   },
   {
     id: uuidv4(),
-    parentId: "2",
     type: "MathFloat",
     position: { x: 0, y: 200 },
     data: { initialComputeType: "Addition" },
