f61b1005585acbe891cf3eb5b422d1f8fa069e39
dklein00 <131988668+dklein00@users.noreply.github.com>
2025-05-11 16:14:26 +0200

    raccoon sprite + animation setup

--- app/lib/game/levels/playground.ts
+++ app/lib/game/levels/playground.ts
@@ -6,16 +6,28 @@ export const initializePlayground = () => {
   const k = getKaplayCtx();
 
   k.loadBean();
-  const bean = k.add([
-    k.sprite("bean"),
-    k.color(0, 0, 255),
+  k.loadSprite("raccoon", "app/assets/game/sprites/raccoon_spritesheet.png", {
+    sliceX: 3,
+    sliceY: 3,
+    anims: {
+        idle: { from: 0, to: 0, loop: false },
+        walk: { from: 1, to: 6, loop: true},
+    }
+  });
+  const raccoon = k.add([
+    k.sprite("raccoon", {
+       anim: "walk",
+    }),
     k.pos(100, k.height() - 100),
+    k.scale(10)
   ]);
+  
+  /*
   const bean2 = k.add([
     k.sprite("bean"),
     k.color(0, 255, 0),
     k.pos(100, k.height() - 100),
-  ]);
+  ]);*/
 
   //Create "Floor" Component
   const floor = k.add([
@@ -31,14 +43,16 @@ export const initializePlayground = () => {
     if (useGameStore.getState().isPaused) return;
 
     //Move
-    bean.pos.x =
+    raccoon.pos.x =
       useDataStore.getState().gameObjects.get("bean")?.get("xpos")?.value ?? 0;
-    bean.pos.y =
+    raccoon.pos.y =
       useDataStore.getState().gameObjects.get("bean")?.get("ypos")?.value ?? 0;
 
+      /*
     bean2.pos.x =
       useDataStore.getState().gameObjects.get("bean2")?.get("xpos")?.value ?? 0;
     bean2.pos.y =
       useDataStore.getState().gameObjects.get("bean2")?.get("ypos")?.value ?? 0;
+      */
   });
 };
