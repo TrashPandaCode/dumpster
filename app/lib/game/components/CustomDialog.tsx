9efe8e251e14aeafda2325537a625250f1f27d7f
JonathanKr <post.david04@gmail.com>
2025-05-18 22:00:26 +0200

    make dialog border thinner

--- app/lib/game/components/CustomDialog.tsx
+++ app/lib/game/components/CustomDialog.tsx
@@ -25,7 +25,7 @@ const CustomDialog = ({
       <DialogTrigger asChild>{trigger}</DialogTrigger>
       <DialogPortal>
         <DialogOverlay className="fixed inset-0 bg-slate-800/25" />
-        <DialogContent className="fixed top-1/2 left-1/2 flex w-1/2 -translate-x-1/2 -translate-y-1/2 flex-col gap-6 rounded-lg border-3 border-blue-300 bg-slate-800/95 p-8 font-mono text-white">
+        <DialogContent className="fixed top-1/2 left-1/2 flex w-1/2 -translate-x-1/2 -translate-y-1/2 flex-col gap-6 rounded-lg border-2 border-blue-300 bg-slate-800/95 p-8 font-mono text-white">
           <DialogTitle className="text-center text-4xl font-bold capitalize">
             {title}
           </DialogTitle>
