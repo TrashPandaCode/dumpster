c4d6c598bf9b1b3d65c2a3c8f8aa449dc78ad9e5
JonathanKr <JonathanKr@users.noreply.github.com>
2025-05-26 21:55:28 +0000

    deploy: 74a8e6d540a3aae9a1e133651931f70e071802fb

---
+++ assets/export-BEEK0gu6.js
@@ -0,0 +1,3 @@
+import{t}from"./chunk-AYJ5UCUI-DfzbB4N7.js";const a={title:"Export To Gameobject"},m=`<h1>Export To Gameobject</h1>
+<DocsNodeEditor type="DocsExportToGameobject" />
+`,c={},s=function(o){return Object.keys(o).forEach(function(e){c[e]=o[e]}),t.createElement("div",null,t.createElement("h1",null,"Export To Gameobject"),t.createElement(c.DocsNodeEditor,{type:"DocsExportToGameobject"}))};export{s as ReactComponent,a as attributes,m as html};
---
+++ assets/import-DMZQ84BU.js
@@ -0,0 +1,3 @@
+import{t}from"./chunk-AYJ5UCUI-DfzbB4N7.js";const r={title:"Import From Gameobject"},a=`<h1>Import from Gameobject</h1>
+<DocsNodeEditor type="DocsImportFromGameobject" />
+`,m={},s=function(o){return Object.keys(o).forEach(function(e){m[e]=o[e]}),t.createElement("div",null,t.createElement("h1",null,"Import from Gameobject"),t.createElement(m.DocsNodeEditor,{type:"DocsImportFromGameobject"}))};export{s as ReactComponent,r as attributes,a as html};
