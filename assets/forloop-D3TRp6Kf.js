8e354a9ae2843672214405abde88d0eab0d8232f
JonathanKr <JonathanKr@users.noreply.github.com>
2025-05-26 12:44:22 +0000

    deploy: 8500108761b555b23ac4957dcaa3db79ce89ee69

---
+++ assets/forloop-D3TRp6Kf.js
@@ -0,0 +1,3 @@
+import{t as o}from"./chunk-AYJ5UCUI-DfzbB4N7.js";const a={title:"For Loop"},m=`<h1>For Loop</h1>
+<DocsNodeEditor type="ForLoop" />
+`,n={},l=function(t){return Object.keys(t).forEach(function(e){n[e]=t[e]}),o.createElement("div",null,o.createElement("h1",null,"For Loop"),o.createElement(n.DocsNodeEditor,{type:"ForLoop"}))};export{l as ReactComponent,a as attributes,m as html};
