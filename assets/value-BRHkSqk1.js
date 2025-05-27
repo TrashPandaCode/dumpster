2f81ce0915d6da2c76798c99c056d9c974644672
JonathanKr <JonathanKr@users.noreply.github.com>
2025-05-27 20:05:24 +0000

    deploy: 81133feb9618f942bd2f80576c50e75d66a42489

--- assets/value-BRHkSqk1.js
+++
@@ -1,4 +0,0 @@
-import{t as e}from"./chunk-AYJ5UCUI-DfzbB4N7.js";const u={title:"Value"},s=`<h1>Value</h1>
-<p>The <em>Value</em> node outputs a single float value. It is used to represent a constant value in the graph. The value can be set in the node’s properties.</p>
-<DocsNodeEditor type="Value" />
-`,a={},r=function(t){return Object.keys(t).forEach(function(n){a[n]=t[n]}),e.createElement("div",null,e.createElement("h1",null,"Value"),e.createElement("p",null,"The ",e.createElement("em",null,"Value")," node outputs a single float value. It is used to represent a constant value in the graph. The value can be set in the node’s properties."),e.createElement(a.DocsNodeEditor,{type:"Value"}))};export{r as ReactComponent,u as attributes,s as html};
