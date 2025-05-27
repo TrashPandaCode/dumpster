8f2e885b0b157c6b581c89f1a6b13c31c58eb8b5
JonathanKr <post.david04@gmail.com>
2025-05-27 21:52:36 +0200

    add correct viewport behaviour for playground level

--- app/lib/game/constants.ts
+++ app/lib/game/constants.ts
@@ -4,3 +4,5 @@ const GOAL_FLAG = "goalFlag";
 
 export const GAME_OBJECTS = [RACCOON, TRASHCAN, GOAL_FLAG] as const;
 export type GameObject = (typeof GAME_OBJECTS)[number];
+
+export const RACCOON_SCALE = 5;
