9004877ea7990a2ab49609e5621376f8aac4f29a
JonathanKr <post.david04@gmail.com>
2025-05-28 17:58:44 +0200

    make background not dependend on viewport scale

--- app/lib/game/core/kaplayCtx.ts
+++ app/lib/game/core/kaplayCtx.ts
@@ -1,7 +1,8 @@
 import kaplay, { type GameObj, type KAPLAYCtx, type TimerComp } from "kaplay";
+
 import { state } from "./initGame";
 
-let ctx: { k: KAPLAYCtx, game: GameObj<TimerComp> } | undefined = undefined;
+let ctx: { k: KAPLAYCtx; game: GameObj<TimerComp> } | undefined = undefined;
 
 export function getKaplayCtx(canvas?: HTMLCanvasElement) {
   if (ctx) {
@@ -15,13 +16,14 @@ export function getKaplayCtx(canvas?: HTMLCanvasElement) {
     debug: false,
     debugKey: "f1",
     canvas: canvas,
+    background: "#2b292f",
   });
   const game = k.add([k.timer()]);
 
   ctx = {
     k,
-    game
-  }
+    game,
+  };
 
   return ctx;
 }
