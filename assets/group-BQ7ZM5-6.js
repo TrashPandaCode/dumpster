ed9a0e5845ae5733cfdb9f811d6ef4f53994c3c1
JonathanKr <JonathanKr@users.noreply.github.com>
2025-05-28 15:50:44 +0000

    deploy: 846dbe1ee51b908e70019d321cbec666c96c7f5a

---
+++ assets/group-BQ7ZM5-6.js
@@ -0,0 +1,11 @@
+import{t as e}from"./chunk-AYJ5UCUI-DfzbB4N7.js";const i={title:"Group"},r=`<h1>Group</h1>
+<p>The <em>Group</em> node allows you to organize and encapsulate multiple nodes within a single container.</p>
+<h2>Functionality</h2>
+<ul>
+<li>Nodes dragged into the <em>Group</em> node become its children and are positioned relative to the <em>Group</em> node.</li>
+<li>Nodes dragged out of the <em>Group</em> node are automatically ungrouped.</li>
+<li>Deleting a <em>Group</em> node node will also delete all of its child nodes.</li>
+<li>Duplicating a <em>Group</em> node will duplicate all of its child nodes as well.</li>
+</ul>
+<DocsNodeEditor type="Group" />
+`,t={},u=function(l){return Object.keys(l).forEach(function(n){t[n]=l[n]}),e.createElement("div",null,e.createElement("h1",null,"Group"),e.createElement("p",null,"The ",e.createElement("em",null,"Group")," node allows you to organize and encapsulate multiple nodes within a single container."),e.createElement("h2",null,"Functionality"),e.createElement("ul",null,e.createElement("li",null,"Nodes dragged into the ",e.createElement("em",null,"Group")," node become its children and are positioned relative to the ",e.createElement("em",null,"Group")," node."),e.createElement("li",null,"Nodes dragged out of the ",e.createElement("em",null,"Group")," node are automatically ungrouped."),e.createElement("li",null,"Deleting a ",e.createElement("em",null,"Group")," node node will also delete all of its child nodes."),e.createElement("li",null,"Duplicating a ",e.createElement("em",null,"Group")," node will duplicate all of its child nodes as well.")),e.createElement(t.DocsNodeEditor,{type:"Group"}))};export{u as ReactComponent,i as attributes,r as html};
