ab4e5a5da54b59f257cdb1c4a5d559f21fc9405f
Modryo <markus.heming@mailbox.org>
2025-06-03 19:25:54 +0200

    added ladder element

--- app/lib/game/levels/timeTransform.ts
+++ app/lib/game/levels/timeTransform.ts
@@ -18,10 +18,37 @@ export const initializeTimeTransform = () => {
   k.setCamPos(0, -BACKGROUND_OFFSET);
   k.setCamScale((CAM_SCALE * k.height()) / 947);
 
+  const ladder = game.add([
+    k.rect(0.1, 15),
+    k.opacity(0),
+    k.anchor("bot"),
+    k.pos(-11, -1),
+    k.rotate(0),
+    k.area(),
+    "ladder",
+  ]);
+
+  let isClimbing = false;
+
+  k.onCollideUpdate("raccoon", "ladder", (raccoon) => {
+    isClimbing = false;
+    if (raccoon.pos.y < 0) {
+      isClimbing = true;
+    }
+    if (k.isKeyDown("up") && raccoon.pos.y >= -15) {
+      raccoon.move(0, -5);
+    }
+    if (k.isKeyDown("down") && raccoon.pos.y <= 0) {
+      raccoon.move(0, 5);
+    }
+  });
+
   game.onUpdate(() => {
     if (useGameStore.getState().isPaused) return;
 
-    animPlayer(raccoon!, k, "Input");
+    if (!isClimbing) {
+      animPlayer(raccoon!, k, "Input");
+    }
 
     if (useDataStore.getState().initData) {
       handleReset(raccoon!, 1);
