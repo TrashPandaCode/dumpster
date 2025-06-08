aa5b979e963d3499ce3f3d9391b5fefabf84ed3f
JonathanKr <post.david04@gmail.com>
2025-06-08 14:43:24 +0200

    different level complete style

--- app/lib/core/components/LevelCard.tsx
+++ app/lib/core/components/LevelCard.tsx
@@ -1,3 +1,4 @@
+import { CheckCircledIcon } from "@radix-ui/react-icons";
 import { NavLink } from "react-router";
 
 import difficultyOneImage from "~/assets/D1.png";
@@ -53,14 +54,16 @@ const LevelCard: React.FC<{ level: Level }> = ({ level }) => (
         />
       )}
     </div>
+
+    {JSON.parse(localStorage.getItem(`game-store-${level.slug}`) ?? "{}")
+      .levelCompleted && (
+      <div className="absolute top-0 right-0 z-1 aspect-square w-11 bg-white [clip-path:polygon(100%_100%,0_0,100%_0)]">
+        <CheckCircledIcon className="absolute top-1.5 right-1.5 text-green-700" />
+      </div>
+    )}
+
     <div className="font-pixel absolute top-6 z-1 m-2 flex items-center justify-center rounded-full bg-white px-2 py-0.5 text-[11px]">
       {level.category}
-    </div>
-    <div className="font-pixel absolute top-12 z-1 m-2 flex items-center justify-center rounded-full bg-white px-2 py-0.5 text-[11px]">
-      {JSON.parse(localStorage.getItem(`game-store-${level.slug}`) ?? "{}")
-        .levelCompleted
-        ? "completed"
-        : "not completed"}
     </div>
     <div className="font-pixel bg-jam-600 absolute bottom-0 z-1 flex h-10 w-full items-center justify-center p-1 text-center text-2xl text-white transition-all duration-300 group-hover:h-12">
       {level.name}
